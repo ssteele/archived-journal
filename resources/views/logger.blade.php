ea8bc07c70e1171185c4b591ff159fc7adebb9c5
Steve Steele <steven.harville.steele@gmail.com>
2015-10-18 16:33:42 -0500

    Update form section title (minor)

--- resources/views/logger.blade.php
+++ resources/views/logger.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
 
-                <div class="panel-heading">Log</div>
+                <div class="panel-heading">Journal</div>
 
                 <div class="panel-body">
 
