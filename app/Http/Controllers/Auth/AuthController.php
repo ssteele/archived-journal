b340292305383a0dcc670b4227a7dd982f5862e5
Steve Steele <steven.harville.steele@gmail.com>
2015-10-18 16:59:36 -0500

    Get journal.com working on work com

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -20,6 +20,8 @@ class AuthController extends Controller {
 
 	use AuthenticatesAndRegistersUsers;
 
+	protected $redirectPath = '/';                                  // stop redirecting to '/home' by default
+
 	/**
 	 * Create a new authentication controller instance.
 	 *
--- config/database.php
+++ config/database.php
@@ -53,15 +53,17 @@ return [
 		],
 
 		'mysql' => [
-			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
-			'strict'    => false,
+			'driver'      => 'mysql',
+			'host'        => env('DB_HOST', 'localhost'),
+			'database'    => env('DB_DATABASE', 'forge'),
+			'username'    => env('DB_USERNAME', 'forge'),
+			'password'    => env('DB_PASSWORD', ''),
+			'port'        => env('DB_PORT', '3306'),
+			'charset'     => 'utf8',
+			'collation'   => 'utf8_unicode_ci',
+			'prefix'      => '',
+			'strict'      => false,
+			'unix_socket' => '/Applications/MAMP/tmp/mysql/mysql.sock',
 		],
 
 		'pgsql' => [
